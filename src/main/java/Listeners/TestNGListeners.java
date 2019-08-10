0be2d69ac44a938bd2bde819a9bcb56684ead359
daniel <danielgold885@gmail.com>
2019-08-10 16:31:39 +0300

    implemented stub listeners - tested successfully by running testng.xml

--- src/main/java/Listeners/TestNGListeners.java
+++ src/main/java/Listeners/TestNGListeners.java
@@ -1,49 +1,41 @@
 package Listeners;
 
-import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class TestNGListeners implements ITestListener {
 	
-	public WebDriver driver;
-	
-	//Class Constructor
-	public TestNGListeners(WebDriver driver) {
-		this.driver = driver;
-	}
 	
 	public void onTestStart(ITestResult result) {
-		// TODO Auto-generated method stub		
+		System.out.println("Listener - onTestStart!" + " " + result.getName());	
 	}
    
 	public void onTestSuccess(ITestResult result) {
-		System.out.println("Listener - Test Passed!" + result.getName());
-		driver.getCurrentUrl();			
+		System.out.println("Listener - onTestSuccess!" + " " + result.getName());	
 	}
 
 	public void onTestFailure(ITestResult result) {
-		System.out.println("Listener - Test Failed!" + result.getName());
+		System.out.println("Listener - onTestFailure!"  + " " + result.getName());
 	}
 
 	public void onTestSkipped(ITestResult result) {
-		System.out.println("Listener - Test Failed!" + result.getName());
+		System.out.println("Listener - onTestSkipped!" + " " + result.getName());
 
 	}
 
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		System.out.println("Listener - Test Failed!" + result.getName());
+		System.out.println("Listener - onTestFailedButWithinSuccessPercentage!" + " " +  result.getName());
 
 	}
 
 	public void onStart(ITestContext context) {
-		System.out.println("Listener - Test Failed!" + context.getName());
+		System.out.println("Listener - onStart!" + " " + context.getName());
 
 	}
 
 	public void onFinish(ITestContext context) {
-		System.out.println("Listener - Test suite finished!" + context.getName());
+		System.out.println("Listener - onFinish!" + " " + context.getName());
 		
 	}
 
