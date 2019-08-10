9c4d075d5e7722f5e935f6d2c9d33a72810425b5
daniel <danielgold885@gmail.com>
2019-08-10 10:58:02 +0300

    Third commit

--- src/main/java/Listeners/testNGListners.java
+++ src/main/java/Listeners/testNGListners.java
@@ -1,5 +1,48 @@
 package Listeners;
 
-public class testNGListners {
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.annotations.Test;
+
+
+public class testNGListners implements ITestListener {
+
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 
 }
